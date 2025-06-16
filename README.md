a83f7fa3914625e70531d31910921a7c677b4665
deepanf5 <git@stackblitz.com>
2025-06-16 22:23:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-ya4ubyp8
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/deepanf5/stackblitz-starters-ya4ubyp8)
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { Header } from './app/components/header/header';
+import { Home } from './app/components/home/home';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-header></app-header>
+  `,
+  imports: [Header, Home],
+})
+export class App {
+  name = 'auto bots';
+}
+
+bootstrapApplication(App, {
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
+});
